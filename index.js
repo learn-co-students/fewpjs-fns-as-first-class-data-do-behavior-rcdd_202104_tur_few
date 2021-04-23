e8fccdcc78cf1d571c840a9ccc4a710982cacf36
CeyhunGulbas <cetree61@gmail.com>
2021-04-23 19:38:40 +0300

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timesPa) {
+  const hour = parseInt(timesPa);
+
+  if (hour >= 0 && hour < 12) {
+    return 'Good Morning';
+  }
+  else if (hour >= 12 && hour < 17) {
+    return 'Good Afternoon';
+  }
+  else{
+    return 'Good Evening';
+  }
+  
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(timesPa) {
+  document.querySelector('#greeting').innerText = timesPa;
+}
