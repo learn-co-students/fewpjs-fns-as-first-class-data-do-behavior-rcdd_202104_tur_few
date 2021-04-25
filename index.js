5c08fcd8d00cc8787f84942729ac349c725c18ae
Buse Şentürk <buse.senturk@outlook.com>
2021-04-25 22:26:02 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet (timeString) {
+  const splittedTime = timeString.split(':');
+  const parsed = parseInt(timeString.split(':').[0]);
+  if (parsed<12){
+    return "Good Morning";
+  }
+  else if (parsed>=12 && parsed <=17){
+    return "Good Afternoon";
+  }
+  else if (parsed > 17){
+    return "Good Evening";
+}
+}
+ function displayMessage(TEST){
+  document.getElementById("greeting").innerText="TEST";
+}
+
+/*let a = [];
+void function displayMessage (a) {
+
+  document.getElementById("greeting").innerHTML = a;
+}*/
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
