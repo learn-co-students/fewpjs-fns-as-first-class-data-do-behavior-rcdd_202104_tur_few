ead96c28b64700571599bf54d85f48a95b93fea9
DKay-code <derya.kaygisiz@outlook.com>
2021-04-27 15:28:12 +0300

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet (HHMM) {
+  // const time = greet.split('');
+  const hour = parseInt (HHMM);
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour > 12 && hour < 17){
+    return "Good Afternoon";
+  } else if (hour > 17){
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
